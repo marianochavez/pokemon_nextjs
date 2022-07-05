61fdde9a35c2e316d5477eda61806c4f82c9a075
marianochavez <amarianochavez@gmail.com>
2022-07-05 13:20:06 -0300

    seccion 6

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -132,7 +132,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map( name => ({
       params: { name }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -141,11 +141,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   
   const { name } = params as { name: string };
+  const pokemon = await getPokemonInfo( name );
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   return {
     props: {
-      pokemon: await getPokemonInfo( name )
-    }
+      pokemon
+    },
+    revalidate: 86400 // 1 day
   }
 }
 
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -132,7 +132,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemons151.map( id => ({
       params: { id }
     })),
-    fallback: false
+    fallback: 'blocking' // Fallback to 404 page
   }
 }
 
@@ -141,11 +141,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo( id );
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   return {
     props: {
-      pokemon: await getPokemonInfo( id )
-    }
+      pokemon
+    },
+    revalidate: 86400 // 1 day
   }
 }
 
